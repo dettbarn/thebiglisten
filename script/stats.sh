140477d29ff3aca4dc9e719131f1b28ba9f09504
Michael Dettbarn <michael.dettbarn@physik.uni-wuerzburg.de>
2023-10-01 16:07:23 +0200

    Create correct ranking of top artists.

--- script/stats.sh
+++ script/stats.sh
@@ -14,4 +14,31 @@ function parse_collaborations {
 	sed 's/ feat. \| & \| vs \| mit \| x \| pres. /\n/g' $1
 }
 
-cat $playlists | remixers_as_artists | keep_only_artists | parse_collaborations | sort | uniq -ci | sort
+cat $playlists | remixers_as_artists | keep_only_artists | parse_collaborations | sort | uniq -ci | sed -e 's/^ *//;s/ /,/' | sort -t ',' -k1,1nr -k2,2 | awk -F ',' '{printf("%s,%s\n", $2, $1)}' > counts.csv
+
+minCount=2
+
+counter=0
+artistCount=0
+prevArtistCount=0
+while read line
+do
+  counter=$((counter+1))
+  if [[ $line =~ (.*),([0-9]+) ]]
+  then
+    artistCount="${BASH_REMATCH[2]}"  # extract count
+	if [[ $artistCount -ne $prevArtistCount ]]
+	then
+	  place=$counter  # different count for this artist, so new place opens
+	fi
+	if [[ $artistCount -lt $minCount ]]
+	then
+	  exit 0  # only print down to minCount, ignore lower counts
+	fi
+	else
+	echo "Error"  # this should not happen
+	exit 1
+  fi
+  echo "$place","$line"
+  prevArtistCount=$artistCount
+done <counts.csv
